21ce517ebc2b9fd982f04e0a529c26cb80316a5e
pritesh-1998 <plathia1998@gmail.com>
2022-07-01 15:19:27 +0530

    added info aboy localhost smtp server

--- email/emailsend.py
+++ email/emailsend.py
@@ -8,13 +8,23 @@ email_password = os.environ.get("emailpass")
 # email_address="";
 # email_password="";
 
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()  # identifies ourselves with the mail servers
-    smtp.starttls()  # start TLS
-    smtp.ehlo()  # reidentify ourselves as an encrypted connection
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    # if you using local server for testing
+    # with smtplib.SMTP_SSL('localhost', 1025) as smtp:
+
+    # all these 4 lines will be commented if you are using local server and smtp_ssl
+    # smtp.ehlo()  # identifies ourselves with the mail servers
+    # smtp.starttls()  # start TLS
+    # smtp.ehlo()  # reidentify ourselves as an encrypted connection
     smtp.login(email_address, email_password)  # authenticate login
+
     subject = "first email sent"
     body = "Hello this is my first email sent"
+
     msg = f'Subject: {subject}\n\n{body}\n'
+
+    # python -m smtpd -c DebuggingServer -n localhost:1025
+    # you can also use local server for testing run above command in terminal or cmd
+
     smtp.sendmail(email_address, 'pritesh.l@somaiya.edu', msg)
     print("Email Sent")
