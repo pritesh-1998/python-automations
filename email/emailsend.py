6ac1572cb236d327fc7de9a0ab2b032bac6f213d
pritesh-1998 <plathia1998@gmail.com>
2022-07-01 15:55:55 +0530

    added image sending functionality

--- email/emailsend.py
+++ email/emailsend.py
@@ -1,30 +1,33 @@
+# python -m smtpd -c DebuggingServer -n localhost:1025
+
 import smtplib
 import os
+import imghdr
+from email.message import EmailMessage
 
 email_address = os.environ.get("email")
 email_password = os.environ.get("emailpass")
-# print(email, password)
 
-# email_address="";
-# email_password="";
+msg = EmailMessage()
+msg['Subject'] = 'first email sent'
+msg['From'] = email_address
+msg['To'] = 'pritesh.l@somaiya.edu'
+msg.set_content('Hello this is my first email sent')
+
+# sending single image
+with open('A:\programming codes\python-cyber\email\pic.jpg', 'rb') as f:
+    file_data = f.read()
+    file_type = imghdr.what(f.name)
+    file_name = f.name
+
+msg.add_attachment(file_data, maintype="image",
+                   subtype=file_type, filename=file_name)
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     # if you using local server for testing
     # with smtplib.SMTP_SSL('localhost', 1025) as smtp:
 
-    # all these 4 lines will be commented if you are using local server and smtp_ssl
-    # smtp.ehlo()  # identifies ourselves with the mail servers
-    # smtp.starttls()  # start TLS
-    # smtp.ehlo()  # reidentify ourselves as an encrypted connection
     smtp.login(email_address, email_password)  # authenticate login
 
-    subject = "first email sent"
-    body = "Hello this is my first email sent"
-
-    msg = f'Subject: {subject}\n\n{body}\n'
-
-    # python -m smtpd -c DebuggingServer -n localhost:1025
-    # you can also use local server for testing run above command in terminal or cmd
-
-    smtp.sendmail(email_address, 'pritesh.l@somaiya.edu', msg)
+    smtp.send_message(msg)
     print("Email Sent")
